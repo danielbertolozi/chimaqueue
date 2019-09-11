30ff174f3971caa9c96a0df19def3ed69d30884e
Daniel Bertolozi <daniel.bertolozi@sap.com>
2019-09-11 12:01:33 -0300

    Add basic implementation for a queue

---
+++ src/Chimas/Queue.ts
@@ -0,0 +1,13 @@
+export default class Queue {
+	private guests: string[] = [];
+	constructor(private readonly name: string) {}
+	public add(name: string) {
+		this.guests.push(name);
+	}
+	public getName() {
+		return this.name;
+	}
+	public remove(name: string) {
+		this.guests = this.guests.filter((guest) => guest !== name);
+	}
+};
