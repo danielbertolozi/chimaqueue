5155f8013a9df960af0a9dd1f350e5ff396cc7f6
Daniel Bertolozi <daniel.bertolozi@sap.com>
2019-09-11 13:00:25 -0300

    implement queue mechanism and server

---
+++ src/LocalDefinitions.d.ts
@@ -0,0 +1,7 @@
+export interface SlackPayload {
+  text: string;
+  team_id: string;
+  channel_id: string;
+  user_id: string;
+  bot_id: string;
+}
--- src/main/StartingPoint.ts
+++ src/main/StartingPoint.ts
@@ -1,1 +1,103 @@
-console.log("Hello world");
+import * as Fastify from "fastify";
+import * as Payload from "slack-payload";
+import { AddressInfo } from "net";
+import QueueHolder from "./Chimas/QueueHolder";
+import { SlackPayload } from "../LocalDefinitions";
+const server = Fastify({ logger: true });
+
+const queues = new QueueHolder();
+
+server.post("/new", async (req, reply) => {
+  const payload = new Payload(req.body) as SlackPayload;
+  const name = payload.channel_id;
+  try {
+    queues.create(name);
+    reply.code(200);
+  } catch (e) {
+    server.log.error(e);
+    reply.code(400);
+  }
+  reply.send(`Queue started for channel ${name}`);
+});
+
+server.get("/join", async (req, reply) => {
+  const payload = new Payload(req.body) as SlackPayload;
+  const name = payload.channel_id;
+  const user = payload.user_id;
+  try {
+    queues.get(name).add(user);
+    reply.code(200);
+  } catch (e) {
+    server.log.error(e);
+    reply.code(400);
+  }
+  reply.send(`${name} has joined the queue!`);
+});
+
+server.get("/leave", async (req, reply) => {
+  const payload = new Payload(req.body) as SlackPayload;
+  const name = payload.channel_id;
+  const user = payload.user_id;
+  try {
+    queues.get(name).remove(user);
+    reply.code(200);
+  } catch (e) {
+    server.log.error(e);
+    reply.code(400);
+  }
+  reply.send(`User ${user} has left the queue.`);
+});
+
+server.get("/next", async (req, reply) => {
+  const payload = new Payload(req.body) as SlackPayload;
+  const name = payload.channel_id;
+  let next: string = "";
+  try {
+    next = queues.get(name).whosNext();
+    reply.code(200);
+  } catch (e) {
+    server.log.error(e);
+    reply.code(400);
+  }
+  reply.send(`The next in queue is ${next}.`);
+});
+
+server.get("/who", async (req, reply) => {
+  const payload = new Payload(req.body) as SlackPayload;
+  const name = payload.channel_id;
+  let usersInQueue: string;
+  try {
+    usersInQueue = queues.get(name).getGuestList().join(", ");
+    reply.code(200);
+  } catch (e) {
+    server.log.error(e);
+    reply.code(400);
+  }
+  reply.send(`The following users are in this queue: ${usersInQueue}.`);
+});
+
+server.get("/clear", async (req, reply) => {
+  const payload = new Payload(req.body) as SlackPayload;
+  const name = payload.channel_id;
+  try {
+    queues.get(name).clear();
+    reply.code(200);
+  } catch (e) {
+    server.log.error(e);
+    reply.code(400);
+  }
+  reply.send(`The queue has been cleared!`);
+});
+
+const start = async () => {
+  try {
+    await server.listen(3000);
+    const port = (server.server.address() as AddressInfo).port;
+    server.log.info(`server listening on ${port}`);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
