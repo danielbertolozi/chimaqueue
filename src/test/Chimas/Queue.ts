4520d30971f2e660c4301476dc66d044257ad9d6
Daniel Bertolozi <daniel.bertolozi@sap.com>
2019-09-11 12:15:23 -0300

    tests are now working

--- src/test/Chimas/Queue.ts
+++ src/test/Chimas/Queue.ts
@@ -17,6 +17,10 @@ context("Queue", () => {
       queue.add("joao");
       expect(queue.getGuestList()).to.have.length(1);
     });
+    it("should throw if person already is in queue", () => {
+      queue.add("joao");
+      expect(queue.add("joao")).to.throw();
+    });
   });
   describe("when calling function remove", () => {
     it("should remove person from queue", () => {
@@ -24,6 +28,9 @@ context("Queue", () => {
       expect(queue.getGuestList()).to.have.length(1);
       queue.remove("joao");
       expect(queue.getGuestList()).to.have.length(0);
+    });
+    it("should throw if person doesn't exist", () => {
+      expect(queue.remove("joao")).to.throw();
     });
   });
 });
