060e77ecc8a84fb37feb64f28d3fd971640f417e
Daniel Bertolozi <daniel.bertolozi@sap.com>
2019-09-11 12:18:12 -0300

    refactor

--- src/main/Chimas/Queue.ts
+++ src/main/Chimas/Queue.ts
@@ -2,21 +2,26 @@ export default class Queue {
 	private guests: string[] = [];
 	constructor(private readonly name: string) {}
 	public add(name: string) {
-		if (this.guests.find((g) => g === name)) {
+		if (this.isPersonInQueue(name)) {
 			throw new Error("Person already in queue");
 		}
 		this.guests.push(name);
 	}
+	
 	public getName() {
 		return this.name;
 	}
 	public remove(name: string) {
-		if (!this.guests.find((g) => g === name)) {
+		if (!this.isPersonInQueue(name)) {
 			throw new Error("Person not in queue");
 		}
 		this.guests = this.guests.filter((guest) => guest !== name);
 	}
 	public getGuestList() {
 		return this.guests;
+	}
+
+	private isPersonInQueue(name: string) {
+		return this.guests.find((g) => g === name);
 	}
 };
